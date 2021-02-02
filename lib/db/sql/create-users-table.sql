f6a2413af23e25a3c29b2873414d3fb46c45114b
Paul Coe <pcoe@tesla.com>
2021-02-01 16:14:41 -0800

    Add create-users-table SQL file

---
+++ lib/db/sql/create-users-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL,
+	email_verified BOOLEAN DEFAULT false,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(60) NOT NULL,
+	profile_url VARCHAR(255)
+);
