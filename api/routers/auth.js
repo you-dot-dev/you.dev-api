cc6c3845d2a70057f66bd2bbccda3863be228258
Paul Coe <pcoe@tesla.com>
2021-02-01 16:13:35 -0800

    Implement /auth/register route and integrate bcrypt

--- api/routers/auth.js
+++ api/routers/auth.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const auth = express.Router();
+const bcrypt = require("bcrypt");
+const jsonParse = express.json();
 
-auth.post("/register", (request, response) => {
+const saltRounds = 10;
 
-  response.json({
-    message: "Registration successful."
+/** POST /auth/register
+ */
+auth.post("/register", jsonParse, (request, response) => {
+
+  const { password, email, username } = request.body;
+
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+
+    bcrypt.hash(password, salt, (err, hash) => {
+
+      if (err) { console.error(err) }
+
+      const db = request.app.get("appDb");
+
+      db.execute(
+        `INSERT INTO users(email, username, password) VALUES (?,?,?)`,
+        [email, username, hash],
+        (err, results, fields) => {
+          console.log("err", err);
+          console.log("results:", results);
+          console.log("fields:", fields);
+
+          response.json({
+            message: "Registration successful."
+          });
+
+        }
+      );
+    });
   });
 
-})
+});
 
 module.exports = auth;
