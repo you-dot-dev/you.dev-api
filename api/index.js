21ab764f26bf2e3029f7ee332156420b0b2f44c6
Paul Coe <pcoe@tesla.com>
2021-04-07 12:35:10 -0700

    Add cookie settings and CORS config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-/** Dependencies
+/**
+ * Dependencies
  */
 const express = require("express");
 const session = require("express-session");
@@ -12,15 +13,20 @@ const cors = require("cors");
 const createDatabaseConnections = require("../lib/db/connections");
 const getIssuer = require("../lib/auth/getIssuer");
 
+
 /**
  * Environment
  */
 const {
   GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET
+  GOOGLE_CLIENT_SECRET,
+  SESSION_SECRET,
+  COOKIE_DOMAIN
 } = process.env;
 
-/** Create API
+
+/**
+ * Create API
  */
 const api = express();
 
@@ -50,70 +56,65 @@ async function main() {
 main();
 
 
-
-/** Set CORS policy
+/**
+ * Set CORS policy
  */
 api.use( cors({
   origin: 'http://localhost:8000',
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  credentials: true
 }) );
 
-/** Get Environment Variables
- */
-const {
-  SESSION_SECRET,
-  COOKIE_DOMAIN,
-} = process.env;
 
 const sessionConfig = {
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  store: new MySQLStore(require("../lib/db/config/root")),
+  store: new MySQLStore( require("../lib/db/config/root") ),
 
   cookie: {
     domain: COOKIE_DOMAIN,
     secure: false,
-    httpOnly: false,
-    sameSite: false,
+    httpOnly: true,
+    sameSite: true,
     path: "/",
     maxAge: null
   }
 }
 
 
-
-/** Middleware
+/**
+ * Middleware
  */
-api.use( cors({
-  origin: 'http://localhost:8000',
-  optionsSuccessStatus: 200
-
-}) );
-api.use( session(sessionConfig) );
+api.use(
+  cors({
+    origin: 'http://localhost:8000',
+    optionsSuccessStatus: 200
+  })
+);
+api.use( session( sessionConfig ) );
 api.use( passport.initialize() );
 api.use( passport.session() );
 
 
-
-/** Routes
+/**
+ * Routes
  */
 api.get(`/`, (req, res) => {
   res.json({
     message: "Welcome to You.Dev"
   });
 });
-
 api.use(`/auth`, require("./routers/auth") );
 api.use(`/articles`, require("./routers/articles"));
 api.use(`/user`, require("./routers/user"));
 
 
-/** Listen for traffic
+/**
+ * Listen for traffic
  */
 const DEFAULT_PORT = 7890;
 const PORT = process.env.PORT || DEFAULT_PORT;
-
 api.listen( PORT, () => {
   console.log(`api.you.dev is listening for HTTP on ${PORT}.`);
 });
